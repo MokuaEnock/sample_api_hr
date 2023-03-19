fa7a742bed908abfe982c5098aee8d30cb93a49c
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 02:52:29 +0300

    update join table

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_235018) do
   create_table "roles_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "role_id", null: false
+    t.index ["role_id", "user_id"], name: "index_roles_users_on_role_id_and_user_id"
+    t.index ["user_id", "role_id"], name: "index_roles_users_on_user_id_and_role_id"
   end
 
   create_table "users", force: :cascade do |t|
