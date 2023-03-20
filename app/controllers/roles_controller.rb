10499ea9db8f9a17dfbf6d78181374f0d23e9d17
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 03:19:34 +0300

    add create ethd for roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,17 @@
 class RolesController < ApplicationController
+  def create
+    @role = Role.new(role_params)
+
+    if @role.save
+      render json: @role, status: :created
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:name, :description)
+  end
 end
